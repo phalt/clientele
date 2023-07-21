a3b685eef3a1a0a8e4c0b7ecbb7d4f32dbb24443
Paul Hallett <paulandrewhallett@gmail.com>
2023-07-21 16:04:21 +1200

    Got it working really well

---
+++ src/settings.py
@@ -0,0 +1,3 @@
+from os.path import abspath, dirname
+
+TEMPLATE_ROOT = dirname(dirname(abspath(__file__))) + "/src/template/"
--- test_output/client.py
+++
@@ -1,7 +0,0 @@
-from httpx import Client, Response
-
-client = Client()
-
-
-def _get(url: str) -> Response:
-    client.get(url)
