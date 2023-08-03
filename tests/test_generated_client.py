c2b8ccbbb602797eee91370299bcad529a7e36a9
Paul Hallett <paulandrewhallett@gmail.com>
2023-08-04 11:32:49 +1200

    more tests

--- tests/test_generated_client.py
+++ tests/test_generated_client.py
@@ -106,3 +106,79 @@ def test_complex_model_request_complex_model_request_get(respx_mock: MockRouter)
     assert len(respx_mock.calls) == 1
     call = respx_mock.calls[0]
     assert call.request.url == BASE_URL + mock_path
+
+
+@pytest.mark.respx(base_url=BASE_URL)
+def test_request_data_request_data_post(respx_mock: MockRouter):
+    # Given
+    mocked_response = {"my_input": "test"}
+    mock_path = "/request-data"
+    respx_mock.post(mock_path).mock(
+        return_value=Response(json=mocked_response, status_code=200)
+    )
+    # When
+    data = schemas.RequestDataRequest(my_input="test")
+    response = client.request_data_request_data_post(data=data)
+    # Then
+    assert isinstance(response, schemas.RequestDataResponse)
+    assert response.model_dump() == mocked_response
+    assert len(respx_mock.calls) == 1
+    call = respx_mock.calls[0]
+    assert call.request.url == BASE_URL + mock_path
+
+
+@pytest.mark.respx(base_url=BASE_URL)
+def test_request_data_path_request_data(respx_mock: MockRouter):
+    # Given
+    path_parameter = "param"
+    mocked_response = {"my_input": "test", "path_parameter": path_parameter}
+    mock_path = f"/request-data/{path_parameter}"
+    respx_mock.post(mock_path).mock(
+        return_value=Response(json=mocked_response, status_code=200)
+    )
+    # When
+    data = schemas.RequestDataRequest(my_input="test")
+    response = client.request_data_path_request_data(path_parameter, data=data)
+    # Then
+    assert isinstance(response, schemas.RequestDataAndParameterResponse)
+    assert response.model_dump() == mocked_response
+    assert len(respx_mock.calls) == 1
+    call = respx_mock.calls[0]
+    assert call.request.url == BASE_URL + mock_path
+
+
+@pytest.mark.respx(base_url=BASE_URL)
+def test_request_delete_request_delete_delete(respx_mock: MockRouter):
+    # Given
+    mocked_response = {}
+    mock_path = "/request-delete"
+    respx_mock.delete(mock_path).mock(
+        return_value=Response(json=mocked_response, status_code=200)
+    )
+    # When
+    response = client.request_delete_request_delete_delete()
+    # Then
+    assert isinstance(response, schemas.DeleteResponse)
+    assert len(respx_mock.calls) == 1
+    call = respx_mock.calls[0]
+    assert call.request.url == BASE_URL + mock_path
+
+
+@pytest.mark.respx(base_url=BASE_URL)
+def test_header_request_header_request_get(respx_mock: MockRouter):
+    # Given
+    input_header = "foo"
+    mocked_response = {"x_test": input_header}
+    mock_path = "/header-request"
+    respx_mock.get(mock_path, headers={"x-test": input_header}).mock(
+        return_value=Response(json=mocked_response, status_code=200)
+    )
+    # When
+    headers = schemas.HeaderRequestHeaderRequestGetHeaders(x_test=input_header)
+    response = client.header_request_header_request_get(headers=headers)
+    # Then
+    assert isinstance(response, schemas.HeadersResponse)
+    assert response.model_dump() == mocked_response
+    assert len(respx_mock.calls) == 1
+    call = respx_mock.calls[0]
+    assert call.request.url == BASE_URL + mock_path
