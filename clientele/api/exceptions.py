e62e30dec1044dd75e2f6edce083d37612729837
Paul Hallett <paulandrewhallett@gmail.com>
2026-01-16 13:15:31 +0000

    Apply HTTPBackends to existing code, maintain backwards compat

--- clientele/api/exceptions.py
+++ clientele/api/exceptions.py
@@ -2,16 +2,23 @@
 
 from __future__ import annotations
 
-import httpx
+from clientele.http import response
 
 
 class APIException(Exception):
     """Could not match API response to return type of this function."""
 
     reason: str
-    response: httpx.Response
+    response: response.Response
 
-    def __init__(self, response: httpx.Response, reason: str, *args: object) -> None:
+    def __init__(self, response: response.Response, reason: str, *args: object) -> None:
         self.response = response
         self.reason = reason
         super().__init__(*args)
+
+
+class HTTPStatusError(APIException):
+    """HTTP response returned an error status code (4xx or 5xx)."""
+
+    def __init__(self, response: response.Response, reason: str, *args: object) -> None:
+        super().__init__(response, reason, *args)
